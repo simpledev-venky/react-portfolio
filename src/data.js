34c46282ef4c3caf2d2b97ac3360daeaf6b731ce
Your Name <you@example.com>
2023-04-17 17:03:20 +0530

    v1

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const projects = [
+    {
+      title: "React Editor",
+      subtitle: "Reactjs",
+      description:
+        "A simple coding editor for html,css,js made using react-js",
+      image: "/src/assets/1.png",
+      link: 'https://wandel-editor.netlify.app/',
+    },
+    {
+        title: "React Quotes",
+        subtitle: "Reactjs",
+        description:
+          "A web Application to generate Inspirational quotes with pleasant background made using react-js",
+        image: "/src/assets/2.png",
+        link: 'https://wandel-quotes.netlify.app/',
+      },
+      {
+        title: "React Dictionary",
+        subtitle: "React and Auth0",
+        description:
+      "A web app to implement dictionary for faster word searching made using react-js",
+    image: "/src/assets/3.png",
+    link: "https://wandel-dictionary128.netlify.app",
+  },
+  {
+    title: "password Generator",
+    subtitle: "html,css,javascript",
+    description:
+      "A web app to generate strong password including special characters made using javascript",
+    image: '/src/assets/4.png',
+    link: "https://simpledev-venky.github.io/js-password-generator/",
+  },
+]
+
+export  const skills = ['Reactjs',
+'Javascrit',
+'Html',
+'Css',
+'Git',
+'Tailwindcss']
