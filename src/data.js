ae2f1cf1b4dd1628e735d45ceee00e3ee648ab30
Your Name <you@example.com>
2023-04-25 14:44:04 +0530

    proj img added in firebase

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
       subtitle: "Reactjs",
       description:
         "A web application to write html,css,js and instantly see the output below made using react-js",
-      image: "https://i.ibb.co/5KMjYFH/1.png",
+      image: "https://firebasestorage.googleapis.com/v0/b/portfolio-bc1bf.appspot.com/o/projectImages%2F1.png?alt=media&token=e05da59f-1ce3-4fbd-94c1-ddc3eaa74f06",
       link: 'https://wandel-editor.netlify.app/',
     },
     {
@@ -12,7 +12,7 @@ export const projects = [
         subtitle: "Reactjs",
         description:
           "A web Application to generate Inspirational quotes with pleasant background made using react-js",
-        image: "https://i.ibb.co/myLc8D9/2.png",
+        image: "https://firebasestorage.googleapis.com/v0/b/portfolio-bc1bf.appspot.com/o/projectImages%2F2.png?alt=media&token=2e668c48-8a7a-4881-a2cd-3b8fb9038a92",
         link: 'https://wandel-quotes.netlify.app/',
       },
       {
@@ -20,7 +20,7 @@ export const projects = [
         subtitle: "React and Auth0",
         description:
       "A web app to implement dictionary for faster word searching made using react-js",
-    image: "https://i.ibb.co/LPprqLM/3.png" ,
+    image: "https://firebasestorage.googleapis.com/v0/b/portfolio-bc1bf.appspot.com/o/projectImages%2F3.png?alt=media&token=9a2bb61b-882d-4ecb-b486-990fbb981b16" ,
     link: "https://wandel-dictionary128.netlify.app",
   },
   {
@@ -28,7 +28,7 @@ export const projects = [
     subtitle: "html,css,javascript",
     description:
       "A web app to generate strong password including special characters made using javascript",
-    image: "https://i.ibb.co/sKyv5kv/4.png" ,
+    image: "https://firebasestorage.googleapis.com/v0/b/portfolio-bc1bf.appspot.com/o/projectImages%2F4.png?alt=media&token=ba9fdc48-8d74-46fd-af8d-8a238926f4b1" ,
     link: "https://simpledev-venky.github.io/js-password-generator/",
   },
 ]
